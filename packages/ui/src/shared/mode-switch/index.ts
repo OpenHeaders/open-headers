88d1cb5e5ac8146e01d1dfa841505f5cc15f81fa
Daniel Tirzuman <github@tirzuman.com>
2026-06-01 14:43:10 +0200

    Remove backup/restore UI: drop restoreFromBackup setting + shared mode-switch capability

--- packages/ui/src/shared/mode-switch/index.ts
+++
@@ -1,4 +0,0 @@
-export { type ExecuteDiscardDeps, executeDiscard } from './execute-discard';
-export { type ExecuteRestoreDeps, executeRestore } from './execute-restore';
-export { summarizeDiscardFailure, summarizeDiscardSuccess } from './summarize-discard';
-export { summarizeRestoreFailure, summarizeRestoreSuccess } from './summarize-restore';
